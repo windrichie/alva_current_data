77acc07a755588fcbb5715065518d4a38d3b75ce
Windrich <windrichie@gmail.com>
2018-02-05 19:09:50 +0800

    Graph + Last 5 measurements

--- library/get_load_data.py
+++ library/get_load_data.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import time
+# import time
 from datetime import datetime
 from flask import Flask, render_template, request
 import io
@@ -18,8 +18,12 @@ app = Flask(__name__)
 def main():
     result = fetch_data()
     data = process_data(result)
-    image_url = plot_data(data)
-    return render_template('graph/images.html', image_url=image_url)
+    image_url, tail_data = plot_data(data)
+    max_index = max(tail_data, key=int)
+    indexes = {max_index, max_index-1, max_index-2, max_index-3, max_index-4}
+    cut_data = dict([(key, tail_data[key]) for key in indexes if key in tail_data])
+    print(cut_data)
+    return render_template('graph/data_layout.html', station_name="Monas Smart Station", image_url=image_url, tail_data=cut_data)
     # time.sleep(5)
 
 
@@ -33,9 +37,6 @@ def fetch_data():
     result = firebase.get('/measurements', None)
     return result
 # print(result)
-
-# f = urlopen('https://console.firebase.google.com/u/1/project/jakartasmartpark/database/jakartasmartpark/data/')
-# objects = ijson.items(result,'LED_status.item')
 
 
 def process_data(result):
@@ -51,7 +52,8 @@ def process_data(result):
         real_timestamp = datetime.strptime(raw_time,'%Y-%m-%dT%H:%M:%S.%f') #convert str into datetime object
         #real_timestamp = datetime.strftime(real_timestamp,'%Y-%m-%d %H:%M:%S')
         if (real_timestamp.year > 2017):
-            raw_data.append(real_timestamp)
+            format_timestamp = datetime.strftime(real_timestamp, '%Y-%m-%d %H:%M:%S')
+            raw_data.append(format_timestamp)
             if 'Grid' in result[f]:
                 raw_data.append(result[f]['Grid'])
             else:
@@ -76,26 +78,25 @@ def process_data(result):
                 raw_data.append(result[f]['Tablet2'])
             else:
                 raw_data.append('NaN')(())
-            data.append(raw_data) #create a new dictionary for the data
+            data.append(raw_data)  # create a new dictionary for the data
+
     return data
 
 
 def plot_data(data):
     status = pd.DataFrame(data, columns=["timestamp", "Grid", "Inverter", "Load", "Rpi_Ard_sensors", "Tablet1", "Tablet2"])
+    tail_dict = status.to_dict('index')
     status = status.set_index('timestamp')
-    # print(status.tail())
     # print(status['timestamp'].dtype)
     # status = status.loc[:,['Grid','Inverter','Load']]
-    # print(status.head())
     status = status.astype(float)
-
     img = io.BytesIO()
     # status = status.cumsum()
-    ax = status.plot()
+    status.plot()
     # plt.figure(); ax; plt.legend(loc='best')
     # plt.show()
     plt.savefig(img, format='png')
     img.seek(0)
     plot_url = base64.b64encode(img.getvalue()).decode()
     print(plot_url)
-    return 'data:image/png;base64,{}'.format(plot_url)
+    return 'data:image/png;base64,{}'.format(plot_url), tail_dict
